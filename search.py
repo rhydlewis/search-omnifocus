a48cf134e96f22b9735a3a0c643b734d6f5b7e79
rhydlewis <rlewis@equalexperts.com>
2015-03-30 11:11:06 +0100

    Refactored in preparation for additional queries

--- search.py
+++ search.py
@@ -1,12 +1,11 @@
-__author__ = 'Rhyd Lewis'
-
 import sqlite3
 import sys
 import re
 import os
 
 from workflow import Workflow, ICON_INFO
-from task import Task
+import taskfactory
+import queries
 
 DB_LOCATION = ("/Library/Containers/com.omnigroup.OmniFocus2/"
                "Data/Library/Caches/com.omnigroup.OmniFocus2/OmniFocusDatabase2")
@@ -37,14 +36,7 @@ def main(workflow):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
 
-    # Hat tip to markokaestner for this SQL... https://github.com/markokaestner/of-task-actions
-    stm_select = "t.persistentIdentifier, t.name, t.dateCompleted, t.blocked, c.name, p.name, t.flagged, t.dateToStart "
-    stm_from = ("((task tt left join projectinfo pi on tt.containingprojectinfo=pi.pk) t left join "
-                "task p on t.task=p.persistentIdentifier) left join context c on t.context = c.persistentIdentifier")
-    # stm_where = ("t.blocked = 0 AND t.childrenCountAvailable = 0 AND t.blockedByFutureStartDate = 0 "
-    stm_where = ("t.childrenCountAvailable = 0 AND t.blockedByFutureStartDate = 0 "
-                 "AND t.dateCompleted IS NULL AND lower(t.name) LIKE lower('%{0}%')".format(query))
-    sql = "SELECT {0} FROM {1} WHERE {2}".format(stm_select, stm_from, stm_where)
+    sql = queries.search_for_task_query(query)
 
     log.debug(sql)
 
@@ -54,26 +46,11 @@ def main(workflow):
     log.debug("Found {0} results".format(len(rows)))
 
     for row in rows:
-        t = create_task(row)
-        # log.debug(str(t))
-
-        icon = ICON_TASK
-
-        if t.is_blocked == 1:
-            icon = ICON_INFO
-
-        workflow.add_item(title=t.name, subtitle=t.project, icon=icon, arg=t.persistent_id, valid=True)
+        log.debug(row)
+        t = taskfactory.create_task(row)
+        workflow.add_item(title=t.name, subtitle=t.project, icon=t.icon, arg=t.persistent_id, valid=True)
 
     wf.send_feedback()
-
-
-def create_task(row):
-    log.debug(row)
-    n = row[1]#.encode('utf8', 'ignore')
-    p = row[5]#.encode('utf8', 'ignore')
-    return Task(persistent_id=row[0], name=n, is_complete=row[2], is_blocked=row[3], context=row[4],
-                project=p)
-
 
 if __name__ == u"__main__":
     wf = Workflow()
