a1ed59f6c6063ae965a09b17413eeed00a0fb2e5
Rhyd Lewis <rlewis@equalexperts.com>
2015-08-25 16:08:39 +0100

    Tidied up SQL statements

--- queries.py
+++ queries.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+NAME_SORT = "name ASC"
+
 TASK_SELECT = ("t.persistentIdentifier, t.name, t.dateCompleted, "
                "t.blockedByFutureStartDate, c.name, p.name, t.flagged, t.dateToStart, "
                "t.inInbox, t.effectiveInInbox, t.effectiveDateToStart, t.childrenCountAvailable, "
@@ -7,68 +9,59 @@ TASK_SELECT = ("t.persistentIdentifier, t.name, t.dateCompleted, "
 TASK_FROM = ("((task tt left join projectinfo pi on tt.containingprojectinfo=pi.pk) t left join "
              "task p on t.task=p.persistentIdentifier) left join "
              "context c on t.context = c.persistentIdentifier")
-TASK_NAME_WHERE = "lower(t.name) LIKE lower('%{0}%')"
+TASK_NAME_WHERE = "t.dateCompleted IS NULL AND lower(t.name) LIKE lower('%{0}%') "
 ACTIVE_CLAUSE = "t.blocked = 0 AND "
 CTX_SELECT = "persistentIdentifier, name, allowsNextAction, active, availableTaskCount"
 
 
 def search_tasks(active_only, query):
-    # stm_where = ("t.childrenCountAvailable = 0 AND "
-    stm_where = ("(t.effectiveInInbox = 0 AND t.inInbox = 0) AND "
-                 "t.dateCompleted IS NULL AND ")
-    stm_where = (stm_where + TASK_NAME_WHERE).format(query)
-    stm_ignore_projects = "t.containingProjectInfo <> t.persistentIdentifier"
+    where = "AND (t.effectiveInInbox = 0 AND t.inInbox = 0) AND " \
+            "t.containingProjectInfo <> t.persistentIdentifier "
+    where = (TASK_NAME_WHERE + where).format(query)
 
     if active_only:
-        stm_where = ("(t.blocked = 0 AND t.blockedByFutureStartDate = 0) AND " + stm_where)
+        where = "(t.blocked = 0 AND t.blockedByFutureStartDate = 0) AND " + where
 
-    return "SELECT {0} FROM {1} WHERE {2} AND {3}".format(TASK_SELECT, TASK_FROM, stm_where,
-                                                          stm_ignore_projects)
+    return _generate_query(TASK_SELECT, TASK_FROM, where, "t." + NAME_SORT)
 
 
 def search_inbox(query):
-    stm_where = ("(t.effectiveInInbox = 1 OR t.inInbox = 1) AND "
-                 "t.dateCompleted IS NULL AND ")
-    stm_where = (stm_where + TASK_NAME_WHERE).format(query)
-    return "SELECT {0} FROM {1} WHERE {2}".format(TASK_SELECT, TASK_FROM, stm_where)
+    where = "AND (t.effectiveInInbox = 1 OR t.inInbox = 1)"
+    where = (TASK_NAME_WHERE + where).format(query)
+    return _generate_query(TASK_SELECT, TASK_FROM, where, "t." + NAME_SORT)
 
 
 def search_projects(active_only, query):
-    stm_select = ("p.pk, t.name, p.status, p.numberOfAvailableTasks, p.numberOfRemainingTasks, "
-                  "p.containsSingletonActions, f.name, t.dateToStart, t.effectiveDateToStart")
-    stm_from = ("(ProjectInfo p LEFT JOIN Task t ON p.task=t.persistentIdentifier) "
-                "LEFT JOIN Folder f ON p.folder=f.persistentIdentifier")
-    stm_where = "lower(t.name) LIKE lower('%{0}%')".format(query)
-    stm_order = "p.containsSingletonActions DESC, t.name ASC"
+    select = ("p.pk, t.name, p.status, p.numberOfAvailableTasks, p.numberOfRemainingTasks, "
+              "p.containsSingletonActions, f.name, t.dateToStart, t.effectiveDateToStart")
+    from_ = ("(ProjectInfo p LEFT JOIN Task t ON p.task=t.persistentIdentifier) "
+             "LEFT JOIN Folder f ON p.folder=f.persistentIdentifier")
+    where = "lower(t.name) LIKE lower('%{0}%')".format(query)
+    order_by = "p.containsSingletonActions DESC, t.name ASC"
 
     if active_only:
-        stm_where = ("p.status = 'active' AND " + stm_where)
+        where = "p.status = 'active' AND " + where
 
-    return "SELECT {0} FROM {1} WHERE {2} ORDER BY {3}".format(stm_select, stm_from, stm_where,
-                                                               stm_order)
+    return _generate_query(select, from_, where, order_by)
 
 
 def search_contexts(query):
-    stm_select = "persistentIdentifier, name, allowsNextAction, active, availableTaskCount"
-    stm_from = "Context"
+    select = "persistentIdentifier, name, allowsNextAction, active, availableTaskCount"
+    where = "active = 1"
     if query:
-        stm_where = " AND lower(name) LIKE lower('%{0}%')".format(query)
-    else:
-        stm_where = ''
-    stm_order = "name ASC"
+        where = where + " AND lower(name) LIKE lower('%{0}%')".format(query)
 
-    return "SELECT {0} FROM {1} WHERE active = 1 {2} ORDER BY {3}".format(stm_select, stm_from,
-                                                                          stm_where, stm_order)
+    return _generate_query(select, "Context", where, NAME_SORT)
 
 
 def search_folders(query):
-    stm_select = "persistentIdentifier, name, active, effectiveActive"
-    stm_from = "Folder"
+    select = "persistentIdentifier, name, active, effectiveActive"
+    where = "(active = 1 OR effectiveActive = 1)"
     if query:
-        stm_where = " AND lower(name) LIKE lower('%{0}%')".format(query)
-    else:
-        stm_where = ''
-    stm_order = "name ASC"
+        where = where + " AND lower(name) LIKE lower('%{0}%')".format(query)
 
-    return "SELECT {0} FROM {1} WHERE (active = 1 OR effectiveActive = 1) {2} ORDER BY {3}".\
-        format(stm_select, stm_from, stm_where, stm_order)
+    return _generate_query(select, "Folder", where, NAME_SORT)
+
+
+def _generate_query(select, from_, where, order_by):
+    return "SELECT {0} FROM {1} WHERE {2} ORDER BY {3}".format(select, from_, where, order_by)
